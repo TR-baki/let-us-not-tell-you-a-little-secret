1cea67b3bd120b189e01269f94690e6a29d4d9f6
Blagoja Stamatovski <blagoja.stamatovski@traderepublic.com>
2022-07-21 14:52:07 +0200

    Add initial security configuration

--- src/main/kotlin/hack/a/ton/secrets/SecretsApplication.kt
+++ src/main/kotlin/hack/a/ton/secrets/SecretsApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class SpringBootIntegrationWithAwsSecretsManagerApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootIntegrationWithAwsSecretsManagerApplication>(*args)
+    runApplication<SpringBootIntegrationWithAwsSecretsManagerApplication>(*args)
 }
---
+++ src/main/kotlin/hack/a/ton/secrets/security/http/SecurityConfiguration.kt
@@ -0,0 +1,62 @@
+package hack.a.ton.secrets.security.http
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.util.matcher.AndRequestMatcher
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+//    @Bean
+//    @Order(Ordered.LOWEST_PRECEDENCE)
+//    fun defaultSecurityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+//        return httpSecurity.cors().and()
+//            .exceptionHandling().authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)).and()
+//            .requestMatchers().anyRequest().and()
+//            .authorizeRequests().anyRequest().authenticated().and()
+//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//            .formLogin().disable()
+//            .logout().disable()
+//            .httpBasic().disable()
+//            .anonymous().disable()
+//            .csrf().disable()
+//            .build()
+//    }
+//
+//    @Bean
+//    @Order(Ordered.LOWEST_PRECEDENCE - 1)
+//    fun actuatorFilterChain(
+//        httpSecurity: HttpSecurity,
+//        @Value("\${management.server.port}") managementPort: Int
+//    ): SecurityFilterChain {
+//
+//        val actuatorRequestMatcher = AndRequestMatcher(
+//            RequestMatcher { it.localPort == managementPort },
+//            AntPathRequestMatcher("/actuator/**")
+//        )
+//
+//        httpSecurity
+//            .requestMatchers { it.requestMatchers(actuatorRequestMatcher) }
+//            .authorizeRequests().anyRequest().permitAll().and()
+//            .sessionManagement().disable()
+//            .formLogin().disable()
+//            .logout().disable()
+//            .httpBasic().disable()
+//            .csrf().disable()
+//            .cors().disable()
+//
+//        return httpSecurity.build()
+//    }
+}
